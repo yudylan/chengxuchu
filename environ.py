de039a20d984aa6fc228a3e84aad9dc21a961cba
fei_yuaner <34877009+yudylan@users.noreply.github.com>
2019-05-30 10:55:28 +0800

    Update environ.py

--- environ.py
+++ environ.py
@@ -13,23 +13,28 @@ class Environment():
     s_dim = 2
     a_dim = 3
     #a_bound = [-1,1]
-    a_bound[0] = [0,800]    ###出力者B的出力上下限,可为负值，即可做正功，也可以做负功
-    a_bound[1] = [-500,500]    ###出力者C的出力上下限
-    a_bound[2] = [0,3000]    ###出力者D的出力上下限
+    a_bound[0] = [0,1200]    ###发电机的出力上下限,也就是发电机的最大功率是1200KW
+    a_bound[1] = [-800,800]    ###蓄电池的出力上下限，最大功率是800KW，也就是充电最大功率是800KW，放电最大功率也是800KW
+    a_bound[2] = [-3000,3000]    ###上级电网出力的上下限，最大功率是3000KW，，也就是买电的上限是3000KW，卖电的上限也是3000KW
     fb_output = []
     fc_output = []
     fd_output = []
     def __init__(self):
-        self.current_soc = 0.6
         self.time_step = 0
-        self.load = pd.read_csv('load.csv')   ###load为总电量需求
+        
+        self.current_soc = 0.6
+        self.load = pd.read_csv('load.csv')   ###load为总电量需求  ，数据读取需要切片
         self.pv = pd.read_csv('pv.csv')   ###pv为光伏发电量
+        self.dianjiabuy = pd.read_csv('dianjiabuy.csv')   ##dianjiabuy为从上级电网买1度电的电价，为分时电价，也就是一天的电价会随时间变化而变化
+        self.dianjiasell = pd.read_csv('dianjiasell.csv')   ##dianjiasell为卖给上级电网1度电的电价，，为也是分时电价
+        ##  峰时段为10:00-15:00、18:00-21:00；平时段为07:00-10:00、15:00-18:00、21:00-23:00；谷时段为00:00-07:00、23:00-24:00
+       
         self.reset()
         
 
     def reset(self):
-        self.current_b = 0.6
-        self.state = [self.load, self.pv, self.current_b]
+        self.current_soc = 0.6
+        self.state = [self.load, self.pv, self.current_soc]
         self.action = [self.fb_output, self.fc_output, self.fd_output]
 
         return np.array(self.state)
@@ -47,7 +52,7 @@ class Environment():
         
     def fdpower(self, action):
         #fd_output = action[2] * capacityd        ####若将出力范围统一变换到[-1,1]，capacityd为蓄电池出力的上下界的差值
-        fd_output = action[2]      ###action[2]直接表示从上级电网的购电量
+        fd_output = action[2]      ###action[2]直接表示上级电网的出力值，为正时，是从上级电网的买电量；为负时，是卖给上级电网的电量
         return fd_output   
         
     load = pv + fb_output + fc_output + fd_output    ####电能平衡，即总用电量=总发电量   (注：购电量也称作为发电量吧)
@@ -64,7 +69,10 @@ class Environment():
         #########蓄电池soc的更新公式
         self.current_c = self.current_soc - fc_output / capacity  ##蓄电池的容量为capacity
         
-        reward_chengben = 10 * fb_output + dianjia * fd_output   ##10为发电机发1度电的成本，dianjia为从上级电网买1度电的成本
+        if fd_output >= 0:
+            reward_chengben = 10 * fb_output *fb_output + dianjiabuy * fd_output   ##10为发电机发1度电的成本，dianjiabuy为从上级电网买1度电的成本
+        eles
+            reward_chengben = 10 * fb_output *fb_output + dianjiasell * fd_output   ##10为发电机发1度电的成本，dianjiasell为卖1度电的利润
         
         if abs(self.current_b - 0.5) > 0.3:
             reward_penalty = 300.0 * (abs(self.current_b - 0.5) + 0.7)    ##将soc限制在[0.2,0.8]的范围内，若超出此范围，则有处罚成本
